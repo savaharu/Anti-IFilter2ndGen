b9f5acf5f96ab42e2946e2cc1d97d0416ec1e596
SVHR <82580739+savaharu@users.noreply.github.com>
2022-12-19 08:19:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 let isCorrectPass = false;
 
-document.getElementById('id_return').onclick = () => {
-
-    window.location.replace('../../index.html');
-
-}
-
 while(!isCorrectPass) {
     let pass = window.prompt('”パスワードを入力してください。”');
     if(pass == 'パスワード') {isCorrectPass = true; window.alert('ようこそ！');}
